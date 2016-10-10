eb5f581268b84cdd31e4bb4bdbe087f39dce90b3
Peter Adeojo <PAdeojo@ggc.edu>
2016-10-10 13:59:55 -0400

    Add files via upload

---
+++ src/Entity.java
@@ -0,0 +1,63 @@
+/**
+ * Class: Entity.java
+ * 
+ * @author Peter Adeojo
+ * @version 1.0
+ *          <p>
+ * Course : ITEC 3150 Spring 2016 
+ * Written: Oct 10, 2016
+ * 
+ * 
+ *  This class – This class describes the common attributes and methods
+ *          of the Entity.java
+ * 
+ *  Purpose: – ******
+ */
+
+public class Entity
+{
+	//Attributes
+	private int health;
+	private int strength;
+	//Construct
+	public Entity()
+	{
+		this.health = health;
+		this.strength = strength;
+	}
+
+	//getters and setters
+	public int getHealth()
+	{
+		return health;
+	}
+
+	public void setHealth(int health)
+	{
+		this.health = health;
+	}
+
+	public int getStrength()
+	{
+		return strength;
+	}
+
+	public void setStrength(int strength)
+	{
+		this.strength = strength;
+	}
+	
+	
+	//methods
+	public void attack(Entity target)
+	{
+		
+		
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/Player.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class: Player.java
+ * 
+ * @author Peter Adeojo
+ * @version 1.0
+ *          <p>
+ * Course : ITEC 3150 Spring 2016 
+ * Written: Oct 10, 2016
+ * 
+ * 
+ *  This class – This class describes the common attributes and methods
+ *          of the Player.java
+ * 
+ *  Purpose: – ******
+ */
+
+public class Player extends Entity
+{
+	private int gold;
+	private List<Integer> inv;
+	
+	
+	
+
+	public void move()
+	{
+		
+	}
+	
+	public void use(Usable useItem)
+	{
+		
+	}
+	
+	public void equip(Inventory Subsystem.Weapon newWeapon) {
+
+	}
+
+	public void get(Inventory Subsystem.Item newItem) {
+
+	}
+
+	public void buy(Inventory Subsystem.Item newItem) {
+
+	}
+
+	public void retreat() {
+
+	}
+
+	public void look(Rooms Subsytem.Room roomID) {
+
+	}
+
+	public void examine(Rooms Subsytem.Puzzle puzzleID) {
+
+	}
+
+	public void examine(Entity Subsystem.Monster monster) {
+
+	}
+
+}
