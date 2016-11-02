0b16691b14bce552166a86011f13351a26cc6143
Cristian <Cristian@10.251.7.51>
2016-11-02 10:22:42 -0400

    Item Class
    Created an ArrayList named items to store items picked up during
    gameplay.
    Also created some methods to add an item to the ArrayList in case the
    player decides to add or pick up an item during the game. One of the
    methods could be linked by keyword.

--- src/Inventory/Item.java
+++ src/Inventory/Item.java
@@ -1,8 +1,19 @@
 package Inventory;
 
+import java.util.ArrayList;
 
+//TODO
+/*
+ * 
+ *text file that has all the items
+ *item 1, potion, heals this much
+ *
+ * parse that text file so that when it is called item1 it can be called back.
+ * */
 
 public class Item {
+
+	private ArrayList<Item> items;
 
 	private String name;
 
@@ -12,46 +23,51 @@ public class Item {
 
 	private int itemID;
 
-	public String getName()
-	{
+	public String getName() {
 		return name;
 	}
 
-	public void setName(String name)
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
 
-	public int getPrice()
-	{
+	public int getPrice() {
 		return price;
 	}
 
-	public void setPrice(int price)
-	{
+	public void setPrice(int price) {
 		this.price = price;
 	}
 
-	public String getDescription()
-	{
+	public String getDescription() {
 		return description;
 	}
 
-	public void setDescription(String description)
-	{
+	public void setDescription(String description) {
 		this.description = description;
 	}
 
-	public int getItemID()
-	{
+	public int getItemID() {
 		return itemID;
 	}
 
-	public void setItemID(int itemID)
-	{
+	/*
+	 * The item Id should be pulled from the ArrayList
+	 */
+	public void setItemID(int itemID) {
 		this.itemID = itemID;
 	}
-	
-	
 
+	// method to add items to item array
+	public void addItem(Item obj) {
+		items.add(obj);
+	}
+
+	/*
+	 * In gameplay if the player decides to 'pick up' an item that is available
+	 * then this mthod could be called. and it adds it to the items array.
+	 */
+	public void pickUpItem(Item obj) {
+		items.add(obj);
+	}
 }
